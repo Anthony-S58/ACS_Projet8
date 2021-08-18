669824904a7a4814354d54fd9a73ce346f988432
Anthony-S58 <tonysim01@hotmail.com>
2021-08-18 16:55:49 +0200

    addCSS

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>BAD CORNER - Accueil</title>
+</head>
+<body>
+    <header>
+        <h1>BAD CORNER</h1>
+    </header>
+
+    <div class="categories">
+        <h2>CATEGORIES</h2>
+    </div>
+    
+   <div class="bulles">
+       <div class="bulle1"><div class="content">
+           <div class="text">
+               Ordinateurs
+           </div>
+       </div>
+    </div>
+       <div class="bulle2"><div class="content">
+           <div class="text">
+                Smartphone
+           </div>
+       </div>
+    </div>
+       <div class="bulle3"><div class="content">
+           <div class="text">
+                Musique
+           </div>
+       </div>
+    </div>
+       <div class="bulle4"><div class="content">
+           <div class="text">
+                Gaming
+           </div>
+       </div>
+    </div>
+   </div>
+    
+    <footer>
+        <div class="menu">
+            <h2>
+                <a href="register.php">Inscription</a>
+            </h2>
+            <h2>
+            <a href="connect.php">Connexion</a>
+
+            </h2>
+        </div>
+    </footer>
+</body>
+</html>
