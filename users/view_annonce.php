dc51395b3cc1eb8969c5133fd6f8df2917932076
Anthony-S58 <tonysim01@hotmail.com>
2021-08-20 15:54:45 +0200

    addCSS

--- users/view_annonce.php
+++ users/view_annonce.php
@@ -86,17 +86,35 @@
    
     <footer>
         <div class="menu">
-            <h2>
-                <a href="connect.php">Connexion</a>
-                
-            </h2>
-            <h2>
-                <a href="register.php">Inscription</a>
-            </h2>
+            <div class="accueil_footer">
+                <a href="../index.php">
+                    <i class="fas fa-home"></i>
+                    <p>Accueil</p>
+                </a>
+            </div>
+            <div class="annonces_footer">
+                <a href="mes_annonces.php">
+                    <i class="fas fa-dollar-sign"></i>
+                    <p>Vendre</p>
+                </a>
+            </div>
+            <div class="favoris_footer">
+                <a href="mes_favoris.php">
+                    <i class="far fa-star"></i>
+                    <p>Favoris</p>
+                </a>
+            </div>
+            <div class="profil_footer">
+                <a href="mon_profil.php">
+                    <i class="fas fa-user-alt"></i>
+                    <p>Profil</p>
+                </a>
+            </div>
         </div>
     </footer>
 
 
     <script src="../JS/view.js"></script>
+    <script src="https://kit.fontawesome.com/508ebce8fc.js"></script>
 </body>
 </html>
