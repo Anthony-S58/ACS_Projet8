6de3feb0e4fd370e831e02f9d54acd1c47b1fec2
Anthony-S58 <tonysim01@hotmail.com>
2021-08-23 08:55:54 +0200

    addDbConnect

--- bddconnect.php
+++ bddconnect.php
@@ -4,9 +4,11 @@
 
 <?php
 
+
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=projet8;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+       
 }
 catch (Exception $e)
 {
